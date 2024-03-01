2fe42b307595e45e1c0031b60b11aefd2070d934
Volodymyr Derunov <vderunov@gmail.com>
2024-03-01 14:31:50 +0200

    Replace flex with grid for dapps layout

--- src/renderer/DApps/Dapps.js
+++ src/renderer/DApps/Dapps.js
@@ -1,5 +1,15 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { Box, Button, Flex, Heading, Image, Link, Skeleton, Spinner } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Heading,
+  Image,
+  Link,
+  Skeleton,
+  Spinner,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react';
 import React from 'react';
 import { useDapps } from './useDapps';
 
@@ -16,6 +26,7 @@ function DappButton({ dapp }) {
       rightIcon={dapp.url ? <ExternalLinkIcon /> : <Spinner size="xs" />}
       isDisabled={!dapp.url}
       _hover={{ textDecoration: 'none' }}
+      w="100%"
     >
       {dapp.label}
     </Button>
@@ -25,19 +36,21 @@ function DappButton({ dapp }) {
 export function Dapps() {
   const { data: dapps } = useDapps();
   return (
-    <Box pt="4" px="4" pb="4">
-      <Box flex="1" p="0">
-        <Heading mb="3" size="sm">
-          Available DApps:
-        </Heading>
-        <Flex direction="row" gap={2} justifyContent="start" mb="2" flexWrap="wrap">
-          {dapps.length > 0 ? (
-            dapps.map((dapp) => <DappButton key={dapp.id} dapp={dapp} />)
-          ) : (
-            <Skeleton w="full" height={8} />
-          )}
-        </Flex>
-      </Box>
+    <Box p="4">
+      <Heading mb="3" size="sm">
+        Available DApps:
+      </Heading>
+      {dapps.length > 0 ? (
+        <Grid templateColumns="repeat(3, 1fr)" gap={2} mb={2}>
+          {dapps.map((dapp) => (
+            <GridItem key={dapp.id}>
+              <DappButton dapp={dapp} />
+            </GridItem>
+          ))}
+        </Grid>
+      ) : (
+        <Skeleton height={8} />
+      )}
     </Box>
   );
 }
